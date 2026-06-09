ee3ef7bcbcfd00a22847e05b462126a459b74080
Nelson Rojas <nelson_rojas_n@hotmail.com>
2026-06-09 16:21:39 -0400

    ui: mejora del onboarding de instalación y switch de seguridad

--- pin/pages/default.php
+++ pin/pages/default.php
@@ -1,6 +1,13 @@
 <?php
 
-function index()
+class DefaultPage
 {
-	load_view("default/index", ["saludo" => "Saludo interno"]);
+	public function index()
+	{
+		load_view("default/index", [
+			"saludo" => "¡Bienvenido a Pin Zero!",
+			"db_exists" => file_exists(DB_PATH),
+			"setup_allowed" => defined('ALLOW_SETUP') && ALLOW_SETUP === true
+		]);
+	}
 }
